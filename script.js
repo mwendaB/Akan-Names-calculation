c7a6fb6083ad455a00f62e498005b64fecb99d1f
mwendaB <brian.mwenda@moringaschool.com>
2021-09-04 11:24:26 +0300

    work on js file

---
+++ script.js
@@ -0,0 +1,15 @@
+function submition(){
+
+var day=document.getElementById("day").value;
+var dd=parseInt(day)
+
+var month=document.getElementById("month").value;
+var mm=parseInt(month)
+
+var year=document.getElementById("year").value;
+var yy=parseInt(year)
+
+var cc=(yy-1)/100+1;
+
+
+}
